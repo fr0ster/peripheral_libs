ea81a2c55e16400f7870cb7a5859cecae7230a80
Aleksey Kislitsa <aleksey.kislitsa@gmail.com>
2022-09-16 23:16:58 +0300

    Corrected defines and includes

--- Sensors/ds18b20/onewire.c
+++ Sensors/ds18b20/onewire.c
@@ -16,9 +16,9 @@
  * | along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * |----------------------------------------------------------------------
  */
-#include "ds18b20Config.h"
-#include "tim.h"
+#include "ds18b20.h"
 #ifdef ONEWIRE_ENABLED
+#include "tim.h"
 
 void ONEWIRE_DELAY(uint16_t time_us)
 {
